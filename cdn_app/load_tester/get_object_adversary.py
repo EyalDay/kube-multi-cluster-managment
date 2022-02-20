d0d8644c12a4d0ebb13d51d867799445adea535b
noam <noamzah@gmail.com>
2022-02-20 21:00:49 +0200

    change user story

--- cdn_app/load_tester/get_object_adversary.py
+++ cdn_app/load_tester/get_object_adversary.py
@@ -1,11 +1,16 @@
 from locust import HttpUser, task, between
 
+import time
+
 class GetObjectAdversary(HttpUser):
 
     @task
     def get_object(self):
-        for obj_hash in range(130):
-            self.client.get(f'/get_object?obj_hash={obj_hash}&obj_size_mb=1&obj_ttl_sec=1')
+        for movie in range(2):
+            for segment in range(10):
+                obj_hash = f'{movie}_{segment}'
+                self.client.get(f'/get_object?obj_hash={obj_hash}&obj_size_mb=100&obj_ttl_sec=60')
+                time.sleep(2)
 
     #@task
     def health(self):
--- cdn_app/load_tester/get_object_user.py
+++ cdn_app/load_tester/get_object_user.py
@@ -1,12 +1,17 @@
 from locust import HttpUser, task, between
 import random
+import time
+
 class GetObjectUser(HttpUser):
     wait_time = between(20, 30)
 
     @task
     def get_object(self):
-        obj_hash = random.randint(0, 130)
-        self.client.get(f'/get_object?obj_hash={obj_hash}&obj_size_mb=1&obj_ttl_sec=1')
+        movie = random.randint(0, 2)
+        for movie_segment in range(10):
+            obj_hash = f'{movie}_{movie_segment}'
+            self.client.get(f'/get_object?obj_hash={obj_hash}&obj_size_mb=100&obj_ttl_sec=60')
+            time.sleep(2)
 
     #@task
     def health(self):
